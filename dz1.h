9c10a8901728de0cea7104a39553897dc6bbab49
3 <ricukasovol@gmail.com>
2025-03-23 18:36:44 +0300

    HW_1y_4t_cpp

--- dz1.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstddef> // For size_t
-
-// Function declarations
-void ShowNum_reverse(const int* array, size_t size_ar);
-void ShowNum(const int* array, size_t size_ar);
-void ShowEven(const int* array, size_t size_ar);
