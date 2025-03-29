e5caeacae10c009fb24a3713a88d5c862e41e36b
3 <ricukasovol@gmail.com>
2025-03-29 15:16:28 +0300

    HW_1y_1t_cpp

--- dz1.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Объявление функций
-void ShowNum_reverse(const int* array, size_t size_ar);
-void ShowNum(const int* array, size_t size_ar);
-void ShowEven(const int* array, size_t size_ar);
